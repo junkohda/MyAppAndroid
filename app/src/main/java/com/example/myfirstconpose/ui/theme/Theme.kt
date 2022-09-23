d9993f0fa961534774d5e90472f30025774ec4fc
jun.kohda <>
2022-09-23 11:21:17 +0900

    24まで実装　WIPポートフォリオのアイコン調整

--- app/src/main/java/com/example/myfirstconpose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myfirstconpose/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MyFirstConposeTheme(
+fun JetBizCardTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
